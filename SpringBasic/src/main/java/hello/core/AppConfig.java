84ab8f66984746fb03f9079352608200ef18a9a8
Joe <jhhong0930@gmail.com>
2021-08-03 08:36:42 +0900

    Update: 스프링 빈 등록

--- SpringBasic/src/main/java/hello/core/AppConfig.java
+++ SpringBasic/src/main/java/hello/core/AppConfig.java
@@ -9,7 +9,10 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     /* 리펙토링 전
@@ -22,18 +25,22 @@ public class AppConfig {
     }
     */
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPoilicy();
         return new RateDiscountPolicy();
