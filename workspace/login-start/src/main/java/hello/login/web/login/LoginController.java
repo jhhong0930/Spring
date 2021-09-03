62b0e7d1f9f9d433a3ee034e3f3797b0ff89897a
Joe <jhhong0930@gmail.com>
2021-09-03 10:53:14 +0900

    로그인 처리하기 - 쿠키와 보안 문제

--- workspace/login-start/src/main/java/hello/login/web/login/LoginController.java
+++ workspace/login-start/src/main/java/hello/login/web/login/LoginController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -25,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
 
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -38,7 +40,38 @@ public class LoginController {
             return "login/loginForm";
         }
 
+        // 로그인 성공 처리
+
+        // 쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 소멸)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
+
         return "redirect:/";
     }
 
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+
+    /*
+    쿠키의 보안 문제
+    - 쿠키의 값은 임의로 변경될 수 있다
+    - 클라이언트가 쿠키를 강제로 변경하면 다른 사용자가 된다
+    - 쿠키에 보관된 정보는 훔쳐갈 수 있다
+
+    대안
+    - 쿠키에 중요한 값을 노출하지 않고 사용자 별로 예측 불가능한 임의의 토큰을 노출하고,
+      서버에 토큰과 사용자 id를 매핑해서 인식, 토큰은 서버에서 관리
+    - 해커가 토큰을 가져가도 시간이 지나면 사용할 수 없도록 토큰의 만료시간을 짧게 유지한다
+     */
 }
