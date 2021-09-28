4509d7aaa9eb496da8fe6e077e283e94d4554313
jhhong0930 <jhhong0930@gmail.com>
2021-09-28 09:37:28 +0900

    update: 내용 수정

--- workspace/core/src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ workspace/core/src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -16,13 +16,14 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class ApplicationContextSameBeanFindTest {
+class ApplicationContextSameBeanFindTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
 
     @Test
     @DisplayName("타입으로 조회 시 같은 타입이 둘 이상 있으면 중복 오류가 발생한다")
     void findBeanByTypeDuplicate() {
+
         assertThrows(NoUniqueBeanDefinitionException.class,
                 () -> ac.getBean(MemberRepository.class));
     }
@@ -30,19 +31,24 @@ public class ApplicationContextSameBeanFindTest {
     @Test
     @DisplayName("타입으로 조회 시 같은 타입이 둘 이상 있으면 빈 이름을 지정하면 된다")
     void findBeanByName(){
-        MemberRepository memberRepository = ac.getBean("memberRepository1", MemberRepository.class);
-        assertThat(memberRepository).isInstanceOf(MemberRepository.class);
 
+        MemberRepository memberRepository = ac.getBean("memberRepository1", MemberRepository.class);
+
+        assertThat(memberRepository).isInstanceOf(MemberRepository.class);
     }
 
     @Test
     @DisplayName("특정 타입을 모두 조회하기")
     void findAllBeanByType() {
+
         Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
+
         for (String key : beansOfType.keySet()) {
             System.out.println("key = " + key + " value = " + beansOfType.get(key));
         }
+
         System.out.println("beansOfType = " + beansOfType);
+        
         assertThat(beansOfType.size()).isEqualTo(2);
     }
 
